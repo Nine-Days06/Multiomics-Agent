db39a7d2ff8949f0cde43b4f578da6aaebb841e7
Nine_Day <2281302241@qq.com>
2026-09-20 22:27:03 +0800

    fix(control): 修复 __all__ 导出顺序

--- src/control/__init__.py
+++ src/control/__init__.py
@@ -3,4 +3,4 @@ from .r_script_generator import RScriptGenerator
 from .intent_parser import IntentParser
 from .workflow_manager import WorkflowManager
 
-__all__ = ['RScriptGenerator', 'IntentParser', 'WorkflowManager']
+__all__ = ['IntentParser', 'RScriptGenerator', 'WorkflowManager']
